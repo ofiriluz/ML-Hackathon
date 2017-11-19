3c1b56d8aa117c978ed2cf43747c027ab23d4307
JonVol <jonvol@gmail.com>
2017-11-19 20:27:51 +0200

    Finished initial work on mouse extraction

--- Tests.py
+++ Tests.py
@@ -16,7 +16,9 @@ def pos_generator(mu, stddev):
 
 
 def generate_random_features():
-    with open('C:/Users/ofiri/Desktop/Tests/features.txt') as f:
+
+    #with open('C:/Users/ofiri/Desktop/Tests/features.txt') as f:
+    with open('C:/Users/Jony/RDPML_Tests/features.txt') as f:
         lines = f.readlines()
         features = list(map(lambda x: list(map(lambda y: float(y), x)), [line.split(',') for line in lines]))
         generated_features = []
@@ -24,7 +26,8 @@ def generate_random_features():
         stddev = 0.01
         for i in range(generator_count):
             generated_features.append([pos_generator(arg, stddev) for arg in random.choice(features)])
-        with open('C:/Users/ofiri/Desktop/Tests/generated_features.txt', 'w') as f2:
+        #with open('C:/Users/ofiri/Desktop/Tests/generated_features.txt', 'w') as f2:
+        with open('C:/Users/Jony/RDPML_Tests/generated_features.txt', 'w') as f2:
             for line in generated_features:
                 f2.write(','.join([str(item) for item in line]) + '\n')
 
@@ -48,12 +51,17 @@ def generate_mouse_features():
     extractor = MouseExtractor(data_chunk_duration_sec=5)
     stream.init_stream()
     features = None
-    count = 30
-    f = open('C:/Users/ofiri/Desktop/Tests/mouse_features.txt', 'w')
+    count = 3
+    #f = open('C:/Users/ofiri/Desktop/Tests/mouse_features.txt', 'w')
+    f = open('C:/Users/Jony/RDPML_Tests/mouse_features.txt', 'w')
     for c in range(count):
+        features = None
         while not features:
             features = generate_features(stream, extractor)
-        f.write(','.join(features) + '\n')
+        print("FEATURES=")
+        print(features.get_features())
+        print([str(x) for x in features.get_features().tolist()])
+        f.write(','.join([str(x) for x in features.get_features().tolist()]) + '\n')
     f.close()
     stream.stop_stream()
 
@@ -64,7 +72,8 @@ def generate_keyboard_features():
     stream.init_stream()
     features = None
     count = 30
-    f = open('C:/Users/ofiri/Desktop/Tests/keyboard_features.txt', 'w')
+    #f = open('C:/Users/ofiri/Desktop/Tests/keyboard_features.txt', 'w')
+    f = open('C:/Users/Jony/RDPML_Tests/keyboard_features.txt', 'w')
     for c in range(count):
         while not features:
             features = generate_features(stream, extractor)
@@ -74,12 +83,12 @@ def generate_keyboard_features():
 
 
 if __name__ == '__main__':
-    processor = Processor(sliding_window_time_frame=30,
-                          stddev_threshold=1,
-                          risk_iterations=5,
-                          minimum_training_size=900,
-                          save_trained_model=True,
-                          save_path='./')
+    # processor = Processor(sliding_window_time_frame=30,
+    #                       stddev_threshold=1,
+    #                       risk_iterations=5,
+    #                       minimum_training_size=900,
+    #                       save_trained_model=True,
+    #                       save_path='./')
 
     # Add the weak learners
     # processor.add_weak_learner(learner=AutoEncoderNNWeakLearner(cols_shape=6),
@@ -90,12 +99,13 @@ if __name__ == '__main__':
     #                            extractor=MouseExtractor(),
     #                            data_stream=MouseStream())
 
-    processor.add_weak_learner(learner=AutoEncoderNNWeakLearner(cols_shape=6),
-                               extractor=DataReaderExtractor(),
-                               data_stream=DataReaderStream(data_folder="C:/Users/ofiri/Desktop/Tests"))
+    # processor.add_weak_learner(learner=AutoEncoderNNWeakLearner(cols_shape=6),
+    #                            extractor=DataReaderExtractor(),
+    #                            #data_stream=DataReaderStream(data_folder="C:/Users/ofiri/Desktop/Tests"))
+    #                            data_stream=DataReaderStream(data_folder="C:/Users/Jony/RDPML_Tests"))
 
     # Run the processor
-    processor.start_process()
+    #processor.start_process()
 
-    # generate_mouse_features()
+    generate_mouse_features()
 
